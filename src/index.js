0c6ae99d2a5b36d31aaf27f2e8b197e56b4102bb
jinn <jinnchan@qq.com>
2017-10-28 18:06:02 +0800

    1.跑通路由

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {
+    HashRouter as Router,
+    Route
+} from 'react-router-dom';
+function Root(){
+    return <h1>root</h1>
+}
+function Home(){
+    return <h1>home</h1>
+}
+function User(){
+    return <h1>user</h1>
+}
+function Profile(){
+    return <h1>profile</h1>
+}
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route path="/" component={Root}/>
+            <Route path="/home" component={Home}/>
+            <Route path="/user" component={User}/>
+            <Route path="/profile" component={Profile}/> 
+        </div>  
+    </Router>, document.querySelector('#root')
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+);
