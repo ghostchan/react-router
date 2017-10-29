bb0e39fcca22f440124ec58ec6268910798f3f46
jinn <jinnchan@qq.com>
2017-10-29 10:39:29 +0800

    拆分不同的组件

--- src/index.js
+++ src/index.js
@@ -1,29 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-    HashRouter as Router,
-    Route
-} from 'react-router-dom';
-function Root(){
-    return <h1>root</h1>
-}
-function Home(){
-    return <h1>home</h1>
-}
-function User(){
-    return <h1>user</h1>
-}
-function Profile(){
-    return <h1>profile</h1>
-}
+import App from './components/App';
 ReactDOM.render(
-    <Router>
-        <div>
-            <Route path="/" component={Root}/>
-            <Route path="/home" component={Home}/>
-            <Route path="/user" component={User}/>
-            <Route path="/profile" component={Profile}/> 
-        </div>  
-    </Router>, document.querySelector('#root')
+    App, document.querySelector('#root')
 
 );
